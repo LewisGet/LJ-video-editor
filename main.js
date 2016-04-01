da318b0402baaab23d7fa817a70fadda89120f0e
Lewis Jang <Lewis.AEdes@gmail.com>
2016-04-01 12:24:05 +0800

    init video editor

---
+++ main.js
@@ -0,0 +1,89 @@
+var ljVideoEditor = {
+
+};
+
+ljVideoEditor.getNowTime = function () {
+    return window.videoDisplay.currentTime;
+};
+
+ljVideoEditor.setNowTime = function (val) {
+    return window.videoDisplay.currentTime += val;
+};
+
+ljVideoEditor.isPlaying = function () {
+    return ! (window.videoDisplay.paused);
+};
+
+ljVideoEditor.play = function () {
+    window.videoDisplay.play();
+};
+
+ljVideoEditor.stop = function () {
+    window.videoDisplay.pause();
+};
+
+ljVideoEditor.openVideo = function () {
+    var localUrl = URL.createObjectURL(window.openUrl.files[0]);
+
+    document.title = localUrl;
+
+    window.videoUrl.src = localUrl;
+    window.videoDisplay.load();
+    window.videoDisplay.play();
+
+    // close open panel
+    window.openPanel.style.display = "none";
+
+    // open main panel
+    window.mainPanel.style.display = "";
+
+
+    // set video max height
+    videoDisplay.height = window.innerHeight / 2;
+};
+
+ljVideoEditor.updateTimeController = function () {
+    // console time
+    window.timeStatus.innerHTML = ljVideoEditor.getNowTime();
+
+    var texts = window.texts.getElementsByClassName("text");
+
+    for (var i = 0; i < texts.length; i++)
+    {
+        ljVideoEditor.textDisplay(texts[i]);
+    }
+};
+
+ljVideoEditor.textDisplay = function (text) {
+    text.style.display = "none";
+
+    var startTime = text.getAttribute("data-start");
+    var endTime = text.getAttribute("data-end");
+
+    // 如果現在還沒超過結束時間，而且現在播放時間超過起始時間
+    if (ljVideoEditor.getNowTime() < endTime && ljVideoEditor.getNowTime() > startTime)
+    {
+        text.style.display = "";
+    }
+};
+
+ljVideoEditor.back = function (val) {
+    ljVideoEditor.stop();
+    ljVideoEditor.setNowTime(val * -1);
+};
+
+ljVideoEditor.jump = function (val) {
+    ljVideoEditor.stop();
+    ljVideoEditor.setNowTime(val);
+};
+
+ljVideoEditor.videoClick = function () {
+    if (ljVideoEditor.isPlaying())
+    {
+        ljVideoEditor.stop();
+    }
+    else
+    {
+        ljVideoEditor.play();
+    }
+};
