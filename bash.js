bde972f3682e05af334c2b82e04e10120a005b7a
Lewis Jang <lewis.aedes@gmail.com>
2016-10-17 15:46:09 +0800

    format space

--- bash.js
+++ bash.js
@@ -1,52 +1,52 @@
 var ljBash = {
-	select: []
+    select: []
 };
 
 ljBash.list = function (quickCodeModifies) {
-	var startNumber = 1;
+    var startNumber = 1;
 
-	ljBash.each(function(id) {
-		ljUi.selectTimeBlockDefaultMod(id);
+    ljBash.each(function(id) {
+        ljUi.selectTimeBlockDefaultMod(id);
 
-		ljUi.toDefault(quickCodeModifies(startNumber));
-		ljUi.setEnd();
+        ljUi.toDefault(quickCodeModifies(startNumber));
+        ljUi.setEnd();
 
-		startNumber++;
-	});
+        startNumber++;
+    });
 };
 
 ljBash.leftList = function () {
-	ljBash.list(function(doNumber) {
-		return "l" + doNumber.toString();
-	});
+    ljBash.list(function(doNumber) {
+        return "l" + doNumber.toString();
+    });
 };
 
 ljBash.rightList = function () {
-	ljBash.list(function(doNumber) {
-		return "r" + doNumber.toString();
-	});
+    ljBash.list(function(doNumber) {
+        return "r" + doNumber.toString();
+    });
 };
 
 ljBash.shackAllOn = function () {
-	ljBash.each(function(id) {
-		var amAttr = ljUi.getAmButtonConfig(window.amOneSecShack);
+    ljBash.each(function(id) {
+        var amAttr = ljUi.getAmButtonConfig(window.amOneSecShack);
 
-		ljInput.setBlockStyle(id, amAttr, 1);
-	});
+        ljInput.setBlockStyle(id, amAttr, 1);
+    });
 };
 
 ljBash.shackAllOff = function () {
-	ljBash.each(function(id) {
-		var amAttr = ljUi.getAmButtonConfig(window.amOneSecShack);
+    ljBash.each(function(id) {
+        var amAttr = ljUi.getAmButtonConfig(window.amOneSecShack);
 
-		ljInput.setBlockStyle(id, amAttr, 0);
-	});
+        ljInput.setBlockStyle(id, amAttr, 0);
+    });
 };
 
 ljBash.each = function (excute) {
 
-	// 用開始時間來排序
-	ljBash.select.sort(function(a, b) {
+    // 用開始時間來排序
+    ljBash.select.sort(function(a, b) {
         a = parseFloat(ljInput.getBlockStart(a));
         b = parseFloat(ljInput.getBlockStart(b));
 
@@ -55,16 +55,16 @@ ljBash.each = function (excute) {
             : (a > b ? 1 : -1);
     });
 
-	for (var i = 0; i < ljBash.select.length; i++)
-	{
-		excute(ljBash.select[i]);
-	}
+    for (var i = 0; i < ljBash.select.length; i++)
+    {
+        excute(ljBash.select[i]);
+    }
 };
 
 ljBash.clearSelect = function () {
-	ljBash.each(function(id) {
-		ljUi.selectTimeBlockBashOff(id);
-	});
+    ljBash.each(function(id) {
+        ljUi.selectTimeBlockBashOff(id);
+    });
 
-	ljBash.select = [];
+    ljBash.select = [];
 };
